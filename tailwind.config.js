9a0dc906a43e0bfab0d1732d3a1dbe1e4e9f73d6
AnasPirzada <anaspirzada510@gmail.com>
2024-01-12 02:09:29 +0500

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      backgroundColor : {
+        "primary-color" : "#181A2A",
+        "secondary-color" :"#4B6BFB"
+      },
+      colors : {
+        "primary-text-color ": "#4B6BFB"
+      },
+      fontFamily : {
+       "heading-font" : "Work Sans', sans-serif;"
+      }
+    },
+  },
+  plugins: [],
+}
